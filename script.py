559002dec07bdedf1b7d659591754a99e2b3363e
Abcdviahnibshsjsakakab <96868296+Abcdviahnibshsjsakakab@users.noreply.github.com>
2022-03-03 18:10:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ See <i>/help</i> for commands and more details.</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name :  DQ.BOT_✪࿐ @V</b>
 
 ⭕️<b>Creater :</b> @TroJanzHEX    
 
